dee0715d3c353d14f6049c81a5490853d7a21e9c
Kenny Root <kenny@the-b.org>
2021-02-04 23:54:04 -0800

    Migrate publishing to maven-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,72 @@ buildscript {
     }
 }
 
-allprojects {
+repositories {
+    mavenCentral()
+}
+
+plugins {
+    `java-library`
+    `maven-publish`
+    signing
+    id("net.researchgate.release") version "2.8.1"
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
+}
+
+group = "org.connectbot"
+version = "1.0.1-SNAPSHOT"
+
+val gitHubUrl = "https://github.com/kruton/jbcrypt"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+dependencies {
+    testCompile("junit:junit:4.13.2")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("jBCrypt")
+                description.set("A fork of jBCrypt with more modern OpenBSD algorithms.")
+                url.set(gitHubUrl)
+                licenses {
+                    license {
+                        name.set("ISC")
+                        url.set("https://www.isc.org/downloads/software-support-policy/isc-license/")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Kenny Root")
+                        email.set("kenny@the-b.org")
+                    }
+                }
+                scm {
+                    connection.set("${gitHubUrl}.git")
+                    developerConnection.set("${gitHubUrl}.git")
+                    url.set(gitHubUrl)
+                }
+            }
+        }
+    }
+}
+
+signing {
+    setRequired({
+        gradle.taskGraph.hasTask("publish")
+    })
+    sign(publishing.publications["mavenJava"])
+}
+
+nexusPublishing {
     repositories {
-        mavenCentral()
+        sonatype()
     }
 }
